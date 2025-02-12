410c9b914939a0e08595d909952df43723019494
Harsh Patel <patelharsh9797@gmail.com>
2025-02-12 09:26:03 +0000

    added resend & react-email

---
+++ src/components/email-templates/index.tsx
@@ -0,0 +1,3 @@
+export { default as VerificationEmailTemplate } from "./verification-email";
+export { default as ResetPasswordEmailTemplate } from "./reset-password-email";
+export { default as ChangeEmailVerificationTemplate } from "./change-email-verification";
---
+++ src/server/auth/email.ts
@@ -0,0 +1,57 @@
+import {
+  ChangeEmailVerificationTemplate,
+  ResetPasswordEmailTemplate,
+  VerificationEmailTemplate,
+} from "@/components/email-templates";
+import { env } from "@/env.mjs";
+import { render } from "@react-email/render";
+import { Resend } from "resend";
+
+export const resend = new Resend(env.RESERND_API_KEY);
+
+// export const sendVerificationEmail = async ({
+//   email,
+//   verificationUrl,
+// }: {
+//   email: string;
+//   verificationUrl: string;
+// }) => {
+//   return await resend.emails.send({
+//     from: env.EMAIL_FROM,
+//     to: [email],
+//     subject: "Verify your Email address",
+//     html: await render(
+//       VerificationEmailTemplate({ inviteLink: verificationUrl }),
+//     ),
+//   });
+// };
+
+// export const sendResetPasswordEmail = async ({
+//   email,
+//   verificationUrl,
+// }: {
+//   email: string;
+//   verificationUrl: string;
+// }) => {
+//   return await resend.emails.send({
+//     from: env.EMAIL_FROM,
+//     to: [email],
+//     subject: "Reset Password Link",
+//     react: ResetPasswordEmailTemplate({ inviteLink: verificationUrl }),
+//   });
+// };
+
+// export const sendChangeEmailVerification = async ({
+//   email,
+//   verificationUrl,
+// }: {
+//   email: string;
+//   verificationUrl: string;
+// }) => {
+//   return await resend.emails.send({
+//     from: env.EMAIL_FROM,
+//     to: [email],
+//     subject: "Reset Password Link",
+//     react: ChangeEmailVerificationTemplate({ inviteLink: verificationUrl }),
+//   });
+// };
